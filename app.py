e0f3d033c9079905e6907f05dd46b981843fa741
Jack Ku <sakujjjj@gmail.com>
2023-02-06 17:04:57 +0800

    update user API, login by session

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
+import json
 import mysql.connector
+from mysql.connector import pooling
 
 # 建立 Application 物件
 app = Flask(__name__)
@@ -7,11 +9,28 @@ app = Flask(__name__)
 
 app.secret_key = "any string but secret"
 
+#   Connection pool
+
+connection_pool = pooling.MySQLConnectionPool(
+    pool_name="pynative_pool", pool_size=1, pool_reset_session=True, host='localhost', database='hungfu', user='root', password='1234')
+
+# # Get connection object from a pool
+# connection_object = connection_pool.get_connection()
+# # print(connection_objt)
+# if connection_object.is_connected():
+#     db_Info = connection_object.get_server_info()
+#     print("Connected to MySQL database using connection pool ... MySQL Server version on ", db_Info)
+
+#     cursor = connection_object.cursor()
+#     cursor.execute("select database();")
+#     record = cursor.fetchone()
+#     print("Your connected to - ", record)
+
+
 # 首頁
-
-
 @app.route("/")
 def index():
+
     return render_template("index.html")
 
 
@@ -20,42 +39,40 @@ def index():
 
 @app.route("/signup", methods=["POST"])
 def signup():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="hungfu"
-    )
+    try:
+        connection_object = connection_pool.get_connection()
+        mycursor = connection_object.cursor(buffered=True)
+        name = request.form["name"]
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
+        email = request.form["email"]
+        tsmc_id = request.form["tsmc_id"]
 
-    name = request.form["name"]
-    password = request.form["password"]
-    phone_number = request.form["phone_number"]
-    email = request.form["email"]
-    tsmc_id = request.form["tsmc_id"]
+        sql = "select * from member where phone_number = %s"
+        print(phone_number)
+        mycursor.execute(sql, (phone_number,))
 
-    mycursor = mydb.cursor()
+        results = mycursor.fetchall()
+        print(results)
 
-    sql = "select * from member where phone_number = %s"
-    print(phone_number)
-    mycursor.execute(sql, (phone_number,))
+        # 比對電話是否重複
+        if len(results) == 0:
 
-    # mycursor.execute(sql, (phone_number,))
-    results = mycursor.fetchall()
-    print(results)
-
-    # 比對電話是否重複
-    if len(results) == 0:
-        # member_cursor = mydb.cursor()
-        sql = "insert into member (name, password, phone_number, email, tsmc_id) value (%s, %s, %s, %s, %s)"
-        val = (request.form["name"], request.form["password"],
-               request.form["phone_number"], request.form["email"], request.form["tsmc_id"])
-        mycursor.execute(sql, val)
-        print(val)
-        mydb.commit()
-        print(mycursor.rowcount, "record inserted.")
-        return render_template("success.html")
-    else:
-        return redirect("http://127.0.0.1:3000/error/?message=fail")
+            sql = "insert into member (name, password, phone_number, email, tsmc_id) value (%s, %s, %s, %s, %s)"
+            val = (request.form["name"], request.form["password"],
+                   request.form["phone_number"], request.form["email"], request.form["tsmc_id"])
+            mycursor.execute(sql, val)
+            print(val)
+            connection_object.commit()
+            print(mycursor.rowcount, "record inserted.")
+            return render_template("success.html")
+        else:
+            return redirect("http://127.0.0.1:3000/error/?message=fail")
+    finally:
+        if connection_object.is_connected():
+            mycursor.close()
+            connection_object.close()
+            print("MySQL connection is closed")
 
 
 # 登入頁面
@@ -63,46 +80,38 @@ def signup():
 
 @app.route("/signin", methods=["POST"])
 def signin():
+    try:
+        connection_object = connection_pool.get_connection()
+        mycursor = connection_object.cursor(buffered=True)
 
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="hungfu"
-    )
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
 
-    # name = request.form["name"]
-    password = request.form["password"]
-    phone_number = request.form["phone_number"]
-    # email = request.form["email"]
-    # tsmc_id = request.form["tsmc_id"]
+        # 比對資料庫帳密
+        sql = "select * from member where phone_number = %s and password = %s"
+        mycursor.execute(sql, (phone_number, password))
+        results = mycursor.fetchone()
+        print(results)
 
-    mycursor = mydb.cursor()
+        # print(len(results))
+        if results != None:
+            session["state"] = "login"
+            sql_name = "select name from member where phone_number = %s"
+            mycursor.execute(sql_name, (phone_number,))
+            member_name = results[1]
+            print(member_name)
 
-    # 比對資料庫帳密
-    sql = "select * from member where phone_number='" + \
-        phone_number+"' and password='"+password+"'"
-    mycursor.execute(sql)
-    results = mycursor.fetchall()
-    print(results)
+            # 將姓名存到session
+            session["name"] = member_name
+            return redirect("/member/")
+        else:
+            return redirect("http://127.0.0.1:3000/error/?message=keyerror")
+    finally:
+        if connection_object.is_connected():
+            mycursor.close()
+            connection_object.close()
+            print("MySQL connection is closed")
 
-    print(len(results))
-    if len(results) == 1:
-        session["state"] = "login"
-
-        mycursor = mydb.cursor()
-        sql_name = "select name from member where phone_number='"+phone_number+"'"
-        mycursor.execute(sql_name)
-        member_name = mycursor.fetchall()
-        print(member_name)
-        # 將陣列轉字串
-        member_name = "".join('%s' % id for id in member_name)
-
-        # 將姓名存到session
-        session["value"] = member_name
-        return redirect("/member/")
-    else:
-        return redirect("http://127.0.0.1:3000/error/?message=keyerror")
 
 # 登出系統
 
@@ -118,8 +127,8 @@ def signout():
 @app.route("/member/")
 def member():
     if session["state"] == "login":
-        value = session["value"]
-        return render_template("member.html", value=value)
+        name = session["name"]
+        return render_template("member.html", name=name)
 
     else:
         return render_template("index.html")
@@ -170,16 +179,126 @@ def get_member():
     # dic = {"members": eval(members)}
     # return dic
 
+#   GET USER LOGIN_INFO API
+
+
+@app.route("/api/user", methods=["GET"])
+def get_user():
+    print("phone_number:", session["phone_number"])
+    if session["state"] == "login":
+        try:
+            connection_object = connection_pool.get_connection()
+            mycursor = connection_object.cursor(buffered=True)
+
+            sql = "select * from member where phone_number = %s"
+            mycursor.execute(sql, (session["phone_number"],))
+            results = mycursor.fetchone()
+            print("results:", results)
+            return jsonify({
+                "id": results[0],
+                "name": session["name"],
+                "phone_number": results[3],
+                "email": results[4],
+                "TSMC_ID": results[5],
+                "LEVEL": results[6]
+            })
+        finally:
+            if connection_object.is_connected():
+                mycursor.close()
+                connection_object.close()
+                print("MySQL connection is closed")
+    else:
+        return jsonify({"data": None}), 200
+
+# register a new user API
+
 
 @app.route("/api/user", methods=["POST"])
 def create_user():
-    User_db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="hungfu"
-    )
-    return "ok"
+    try:
+
+        connection_object = connection_pool.get_connection()
+        mycursor = connection_object.cursor(buffered=True)
+        name = request.form["name"]
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
+        email = request.form["email"]
+        tsmc_id = request.form["tsmc_id"]
+        # json_data = request.get_json()
+        # print(json_data)
+        sql = "select * from member where phone_number = %s"
+        print("phone_number:", phone_number)
+        mycursor.execute(sql, (phone_number,))
+
+        results = mycursor.fetchone()
+        print("results:", results)
+
+        # 比對電話是否重複
+        if results == None:
+
+            sql = "insert into member (name, password, phone_number, email, tsmc_id) value (%s, %s, %s, %s, %s)"
+            val = (name, password, phone_number, email, tsmc_id)
+            mycursor.execute(sql, val)
+            print(val)
+            connection_object.commit()
+            print(mycursor.rowcount, "record inserted.")
+            return jsonify({"ok": True}, 200)
+        else:
+            return jsonify({"error": True, "message": "帳號已被註冊"}), 400
+    except:
+        return jsonify({"error": True, "message": "伺服器內部錯誤"}), 500
+    finally:
+        if connection_object.is_connected():
+            mycursor.close()
+            connection_object.close()
+            print("MySQL connection is closed")
+
+#   USER LOGIN API
+
+
+@app.route("/api/user", methods=["PATCH"])
+def login_user():
+    try:
+        connection_object = connection_pool.get_connection()
+        mycursor = connection_object.cursor(buffered=True)
+
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
+
+        # 比對資料庫帳密
+        sql = "select * from member where phone_number = %s and password = %s"
+        mycursor.execute(sql, (phone_number, password))
+        results = mycursor.fetchone()
+        print(results)
+
+        if results != None:
+            session["state"] = "login"
+            sql_name = "select name from member where phone_number = %s"
+            mycursor.execute(sql_name, (phone_number,))
+            session["name"] = results[1]
+            session["phone_number"] = results[3]
+            return redirect("/member/")
+        else:
+            return jsonify({"error": True, "message": "登入失敗!帳號或密碼錯誤"}), 400
+    except:
+        return jsonify({"error": True, "message": "伺服器內部錯誤"}), 500
+    finally:
+        if connection_object.is_connected():
+            mycursor.close()
+            connection_object.close()
+            print("MySQL connection is closed")
+
+#  USER LOGOUT API
+
+
+@app.route("/api/user", methods=["DELETE"])
+def logout():
+    # json_data = request.get_json()
+    # print(json_data)
+    session["state"] = ""
+    session["name"] = ""
+    session["phone_number"] = ""
+    return jsonify({"ok": True}, 200)
 
 
 if __name__ == "__main__":
